68cb46b077c2dc0cb28c59964bc5ae3052b44210
dagm1425 <dagmawinebeyu3@gmail.com>
2024-01-17 18:39:28 +0300

    Add secondary navbar on scroll

--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -1,27 +1,113 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import AnimatedLink from "./AnimatedLink";
 
 const Navbar = () => {
-  const navLinks = [
-    { title: "About", href: "#about" },
-    { title: "Projects", href: "#projects" },
-    { title: "Contact", href: "#contact" },
-  ];
-
-  return (
-    <nav className="fixed top-4 w-full flex justify-center items-center gap-16 text-white text-md uppercase px-8 py-2 z-10">
-      {navLinks.map((link, i) => {
-          return (
-              <div key={i}
-                className="relative">
-                  <AnimatedLink title={link.title}/>
-              </div>
-          )
-        })}
-    </nav>
-  )
+    const [isScrolling, setIsScrolling] = useState(false);
+  
+    const handleScroll = () => {
+      if (window.scrollY >= window.innerHeight) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    };
+  
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+  
+    return (
+      <>
+        <AnimatePresence>
+          {isScrolling ? (
+            <ScrollNavbar isScrolling={isScrolling} />
+          ) : (
+            <FixedNavbar />
+          )}
+        </AnimatePresence>
+      </>
+    );
 }
+
+const FixedNavbar = () => {
+    const navLinks = [
+        { title: "About", href: "#about" },
+        { title: "Projects", href: "#projects" },
+        { title: "Contact", href: "#contact" },
+    ];
+
+    return (
+        <nav className="fixed top-4 w-full flex justify-center items-center gap-16 text-white text-md uppercase px-8 py-2 z-10">
+            {navLinks.map((link, i) => {
+                return (
+                    <div key={i}
+                      className="relative">
+                        <AnimatedLink title={link.title}/>
+                    </div>
+                )
+            })}
+        </nav>
+    )
+}
+
+interface ScrollNavbarProps {
+    isScrolling: boolean;
+}
+
+const ScrollNavbar: React.FC<ScrollNavbarProps> = ({ isScrolling }) => {
+    const navLinks = [
+        { title: "About", href: "#about" },
+        { title: "Projects", href: "#projects" },
+        { title: "Contact", href: "#contact" },
+    ];
+
+    const NavAnimations = {
+        initial: {
+          y: -50,
+          x: "-50%",
+          opacity: 0,
+        },
+        animate: {
+          y: 0,
+          x: "-50%",
+          opacity: 1,
+          transition: {
+            type: "spring",
+            damping: 10,
+            stiffness: 100,
+          },
+        },
+        exit: {
+          y: -50,
+          opacity: 0,
+        },
+    };
+
+    return (
+        <motion.nav
+          key={1}
+          initial="initial"
+          animate={isScrolling ? "animate" : "initial"}
+          exit="exit"
+          variants={NavAnimations}
+          className="fixed left-1/2 top-4 flex justify-center gap-16 text-white text-md bg-[#ffffff14] backdrop-blur-lg uppercase px-8 py-2 border rounded-[32px] border-[#ffffff14] z-10">
+            {navLinks.map((link, i) => {
+                return (
+                    <div key={i}
+                      className="relative">
+                        <AnimatedLink title={link.title}/>
+                    </div>
+                )
+            })}
+        </motion.nav>
+    )
+}
+
 
 export default Navbar;
