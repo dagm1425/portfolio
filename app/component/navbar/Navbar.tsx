68c387e8804920de5c5be73212c1ce2624321fb1
dagm1425 <dagmawinebeyu3@gmail.com>
2024-01-17 15:56:59 +0300

    Add main navigation bar

---
+++ app/component/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+
+const Navbar = () => {
+  const navLinks = [
+    { title: "About", href: "#about" },
+    { title: "Projects", href: "#projects" },
+    { title: "Contact", href: "#contact" },
+  ];
+
+  return (
+      <nav className="fixed top-4 w-full flex justify-center items-center gap-16 text-white text-md uppercase px-8 py-2 z-10">
+          {navLinks.map((link, i) => {
+              return (
+                  <div key={i}>
+                    {link.title}
+                  </div>
+              )
+          })}
+      </nav>
+    )
+}
+
+export default Navbar;
