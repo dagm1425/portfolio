bc0092171d059edaa3163eef5c112fa4c0d84102
dagm1425 <dagmawinebeyu3@gmail.com>
2024-01-24 10:59:14 +0300

    Lazy load spline component

--- app/component/Blob.tsx
+++ app/component/Blob.tsx
@@ -1,22 +1,23 @@
-import React, { useLayoutEffect } from "react";
-// import { Application } from '@splinetool/runtime';
-import Spline from "@splinetool/react-spline";
+import React, { lazy, Suspense } from "react";
 
 interface BlobProps {
   setIsBlobLoading: (value: boolean) => void,
 }
 
+const Spline = lazy(() => delay(import('@splinetool/react-spline')));
+
+const delay = (promise: Promise<any>) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2500);
+  }).then(() => promise);
+}
+
 const Blob: React.FC<BlobProps>  = ({ setIsBlobLoading }) => {
-  // useLayoutEffect(() => {
-  //   const canvas = document.getElementById("canvas3d");
-  //   const app = new Application(canvas as HTMLCanvasElement);
-  //   app.load("https://prod.spline.design/bQ3rzDEw1IxhGK5A/scene.splinecode");
-  // }, []);
-  
   return (
     <div>
-      {/* <canvas id="canvas3d"></canvas> */}
-      <Spline scene="https://prod.spline.design/bQ3rzDEw1IxhGK5A/scene.splinecode" onLoad={() => setIsBlobLoading(false)}/>
+      <Suspense fallback={null}>
+        <Spline scene="https://prod.spline.design/bQ3rzDEw1IxhGK5A/scene.splinecode" onLoad={() => setIsBlobLoading(false)}/>
+      </Suspense>
     </div>
   );
 };
