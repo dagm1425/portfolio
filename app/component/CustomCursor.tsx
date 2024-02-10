36700a90ac76582221c0425eb4e5989b37b5579f
dagm1425 <dagmawinebeyu3@gmail.com>
2024-02-10 11:12:41 +0300

    Adjust scaling

--- app/component/CustomCursor.tsx
+++ app/component/CustomCursor.tsx
@@ -26,7 +26,7 @@ const CustomCursor: React.FC<CustomCursorProps> = ({ isLinkActive, modal }) => {
         <div ref={cursor} className={`
         fixed w-5 h-5 -ml-2 -mt-2 rounded-[50%] transition
         ease-in origin-center z-40 pointer-events-none 
-        ${isLinkActive ? "scale-[4] bg-white border-0 mix-blend-difference" :
+        ${isLinkActive ? "scale-[4.1] bg-white border-0 mix-blend-difference" :
         "scale-100 bg-transparent border border-solid border-white mix-blend-normal"}
         ${modal.isActive ? "hidden" : "block"}
         `}/>
