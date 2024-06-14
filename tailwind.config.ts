2d654517999c0660bf7d183ac022b6482406d492
dagm1425 <dagmawinebeyu3@gmail.com>
2024-06-14 16:41:38 +0300

    Add font for titles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        teko: ['var(--font-teko)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,6 +18,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
 }
 export default config
