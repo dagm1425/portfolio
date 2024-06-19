0978e1ac87785e240021ea1312df7d8a19c6cd05
dagm1425 <dagmawinebeyu3@gmail.com>
2024-06-19 16:37:29 +0300

    Format code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter, Teko } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter, Teko } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const teko = Teko({
-  weight: ['500', '600'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-teko',
+  weight: ["500", "600"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-teko",
 });
 
 export const metadata: Metadata = {
-  title: 'Dagmawi Nebeyu | Front-end Developer',
-}
+  title: "Dagmawi Nebeyu | Front-end Developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${teko.variable} ${inter.className}`}>{children}</body>
     </html>
-  )
+  );
 }
